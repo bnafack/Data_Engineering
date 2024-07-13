bbf2718c3f434f3fbef93fd42b6bd44287c201db
Baurice Nafack <58120702+bnafack@users.noreply.github.com>
2024-07-13 10:40:57 +0200

    Update snwoflake_role_query.sql

--- snwoflake_role_query.sql
+++ snwoflake_role_query.sql
@@ -12,6 +12,14 @@ GRANT ROLE BASIC_ROLE to USER TestUSER;
 GRANT USAGE ON WAREHOUSE COMPUTE_WH to ROLE BASIC_ROLE;
 
 GRANT USAGE on database WS to role BASIC_ROLE;
-grant USAGE on Schema ws.ws_m10050_ltx_03_04 to role basic_role;
+grant USAGE on Schema ws.ws_ts_03_04 to role basic_role;
 
-grant select on table ws.ws_m10050_ltx_03_04.ws_results to role basic_role;
+grant select on table ws.ws_ts_03_04.ws_results to role basic_role;
+
+grant usage on ALL SCHEMAS IN DATABASE WS to role basic_role;
+
+GRANT USAGE on database FT to role BASIC_ROLE;
+grant usage on ALL SCHEMAS IN DATABASE FT to role basic_role;
+GRANT SELECT ON all TABLES IN DATABASE FT TO ROLE basic_role;
+
+GRANT SELECT ON FUTURE TABLES IN DATABASE FT TO ROLE basic_role;
